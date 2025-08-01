90a50da0326e74a2fb6ecf4a017b328f76e464d0
Gurdil Sandhu <gurdilsingh10@gmail.com>
2025-08-01 19:34:19 +0530

    Fix: Ensure react-scripts is installed

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes/Bookroutes');
-const app = express();
 const cors = require('cors');
+const router = require('./routes/Bookroutes');
 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/books", router);
 
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(
-    'mongodb+srv://admin:admin@cluster0.l69zx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-).then(() => console.log("Connected to Database")
-).then(() => {
-    app.listen(5000)
-}).catch(() => console.log(err));
+  'mongodb+srv://admin:admin@cluster0.l69zx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+)
+.then(() => {
+  console.log("Connected to Database");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((err) => console.log(err));
