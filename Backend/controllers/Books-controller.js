3e663d87a1cbc91582c646284482db458e94a980
Gurdil Sandhu <gurdilsingh10@gmail.com>
2025-08-03 21:06:57 +0530

    controller fix

--- Backend/controllers/Books-controller.js
+++ Backend/controllers/Books-controller.js
@@ -1,23 +1,40 @@
 const Books = require("../models/Books");
 
-
 const getAllBooks = async (req, res, next) => {
+    const { search, sortBy, order = "asc" } = req.query;
+
+    let filter = {};
+    if (search) {
+        filter = {
+            $or: [
+                { name: { $regex: search, $options: "i" } },
+                { author: { $regex: search, $options: "i" } }
+            ]
+        };
+    }
+
+    const sortOptions = {};
+    if (sortBy) {
+        sortOptions[sortBy] = order === "desc" ? -1 : 1;
+    }
+
     let books;
     try {
-        books = await Books.find();
+        books = await Books.find(filter).sort(sortOptions);
     } catch (err) {
         console.log(err);
+        return res.status(500).json({ message: "Error fetching books", error: err.message });
     }
 
-    if (!books) {
-        return res.status(404).json({ Message: "No books Found" });
+    if (!books || books.length === 0) {
+        return res.status(404).json({ message: "No books found" });
     }
 
-    return res.status(200).json({ books: books });
-}
+    return res.status(200).json({ books });
+};
 
 const addBook = async (req, res, next) => {
-    const { name, author, description, price, available, image} = req.body;
+    const { name, author, description, price, available, image } = req.body;
     let book;
     try {
         book = new Books({
@@ -32,9 +49,11 @@ const addBook = async (req, res, next) => {
     } catch (err) {
         console.log(err);
     }
+
     if (!book) {
-        return res.status(500).json({ message: "Unable to Add." });
+        return res.status(500).json({ message: "Unable to add book." });
     }
+
     return res.status(201).json({ book });
 };
 
@@ -48,15 +67,15 @@ const getById = async (req, res, next) => {
     }
 
     if (!book) {
-        return res.status(404).json({ Message: "No Book Found" });
+        return res.status(404).json({ message: "No book found" });
     }
 
-    return res.status(200).json({ book: book });
-}
+    return res.status(200).json({ book });
+};
 
 const updateBook = async (req, res, next) => {
     const id = req.params.id;
-    const { name, author, description, price, available, image} = req.body;
+    const { name, author, description, price, available, image } = req.body;
     let book;
     try {
         book = await Books.findByIdAndUpdate(id, {
@@ -66,18 +85,17 @@ const updateBook = async (req, res, next) => {
             price,
             available,
             image,
-        });
-        book = await book.save();
-    }catch(err){
+        }, { new: true });
+    } catch (err) {
         console.log(err);
     }
 
     if (!book) {
-        return res.status(404).json({ message: "Unable to Update."});
+        return res.status(404).json({ message: "Unable to update book." });
     }
 
-    return res.status(201).json({ book: book });
-}
+    return res.status(200).json({ book });
+};
 
 const DeleteBookbyId = async (req, res, next) => {
     const id = req.params.id;
@@ -89,12 +107,13 @@ const DeleteBookbyId = async (req, res, next) => {
     }
 
     if (!book) {
-        return res.status(404).json({ Message: "No Book Deleted" });
+        return res.status(404).json({ message: "No book deleted" });
     }
 
-    return res.status(200).json({message: "Deleted Successfully"});
-}
+    return res.status(200).json({ message: "Deleted successfully" });
+};
 
+// Export all functions
 exports.getAllBooks = getAllBooks;
 exports.addBook = addBook;
 exports.getById = getById;
